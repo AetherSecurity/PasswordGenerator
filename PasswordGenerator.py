e072e3195ac32093103c9b80a98f93d37c1aac39
Arhan <arhanh1234@gmail.com>
2022-11-26 14:23:25 +0400

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -6,23 +6,20 @@ SYMBOLS = ''
 
 def start():
     print('[1] Generate Password\n'
-    + '[2] Settings\n'
-    + '[3] Exit')
+    + '[2] Settings')
     option = input('--> ')
     if option == '1':
         gen()
     elif option == '2':
         settings()
-    elif option == '3':
-        exit()
     else:
-        print("-->Unknown option<--")
+        print("Unknown option!")
 
 def settings():
     global SYMBOLS
     invalid = True
     while invalid:
-        SYMBOLS = input('Do you want to have symbols? ($@!€%&-_.,) y/n: ')
+        SYMBOLS = input('Do you want to have symbols within your password? (Y/N) : ')
         if SYMBOLS == "y" or SYMBOLS == "yes":
             invalid = False
             SYMBOLS = "$@!€%&-_.,"
@@ -30,7 +27,7 @@ def settings():
             invalid = False
             SYMBOLS = ""
         else:
-            return print("-->Unknown option<--")
+            return print("Unknown option!")
 
 def gen():
     global SYMBOLS
@@ -39,16 +36,16 @@ def gen():
         try:
             num = int(input("Password length: "))
             if num >= 257 or num <= 7:
-                print("-->Password length must be between 8-256 characters<--")
+                print("!Password length must be between 8-256 characters!")
         
             else:
                 password = p.generate(num, SYMBOLS)
                 p.copy(password)
                 invalid = False
-                print("-->Password copied to clipboard<--")
+                print("Password copied to clipboard.")
             
         except:
-            print("-->Input was not an integer<--")
+            print("Input was not an integer!")
 
 if __name__ == '__main__':
     while True:
